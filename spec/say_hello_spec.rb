220a63bb012e09777cf97be373fac20ca81b5ac4
Mintra Phophaijit <bluembc@gmail.com>
2019-11-08 18:40:23 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -3,18 +3,7 @@
     puts "Hello #{name}!"
   end
   
+  say_hello("Kent Beck")
   
-  
-  it 'accepts an argument of a name and prints out Hello with that Name' do
-    expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-    say_hello("Kent Beck")
-  end
+say_hello("Ruby Programmer")
 
-
-
-
-  it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello()
-  end
-end
